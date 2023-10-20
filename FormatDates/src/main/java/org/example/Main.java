4aad6387c8cd3f17920ea9d7fd74829a89a39bd3
Valerie Valencia <vvalencia@appdev.yearup.org>
2023-10-20 13:24:33 -0400

    changed time zone format for bonus challenge

--- FormatDates/src/main/java/org/example/Main.java
+++ FormatDates/src/main/java/org/example/Main.java
@@ -10,15 +10,17 @@ public class Main {
         LocalDateTime timeNow = LocalDateTime.now();
         DateTimeFormatter dateTimeSlash = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         DateTimeFormatter dateTimeDash = DateTimeFormatter.ofPattern("yyy-MM-dd");
-        DateTimeFormatter dateTimeChar = DateTimeFormatter.ofPattern("MMM dd, yyyy");
-        DateTimeFormatter dateTimeWeek = DateTimeFormatter.ofPattern("E, MMM dd, yyyy");
-        DateTimeFormatter dateTimeHour = DateTimeFormatter.ofPattern("HH:mm 'on' dd-MMM-yyyy");
+        DateTimeFormatter dateTimeChar = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+        DateTimeFormatter dateTimeWeek = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy");
+        DateTimeFormatter dateTimeHour = DateTimeFormatter.ofPattern("hh:mm 'on' dd-MMM-yyyy");
 
         String dateSlash = timeNow.format(dateTimeSlash);
         String dateDash = timeNow.format(dateTimeDash);
         String dateChar = timeNow.format(dateTimeChar);
         String dateWeek = timeNow.format(dateTimeWeek);
         String dateHour = timeNow.format(dateTimeHour);
+
+        //dateTimeHour.getZone();
 
         System.out.println(dateSlash + "\n" + dateDash + "\n" + dateChar + "\n" + dateWeek + "\n" + dateHour);
     }
